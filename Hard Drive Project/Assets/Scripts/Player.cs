30a191c46d3ba7c752be02fe61fcf0570048daca
Luis Mercado <luisfermepa9@gmail.com>
2023-02-01 00:16:38 -0500

    Player Soul

---
+++ Hard Drive Project/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float jumpForce = 10.0f;
+    private Rigidbody rigidbody;
+    private Vector3 direction;
+    private distanceToGround;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+        direction=Vector3.zero;
+    }
+
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        direction.Set(horizontal, 0, vertical);
+        transform.position += direction * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool IsGrounded() {
+        distanceToGround = GetComponent<Collider>().bounds.extents.y;
+        return Physics.Raycast(transform.position, -Vector3.up, distanceToGround + 0.1f);
+    }
+
+}
